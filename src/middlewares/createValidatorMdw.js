33ad5f650fe698d9f662a01f1d99666ef18fdcc8
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-05-18 21:30:17 -0300

    quitados comments de middleware y todo flama el register

--- src/middlewares/createValidatorMdw.js
+++ src/middlewares/createValidatorMdw.js
@@ -1,41 +1,3 @@
-// const {body} = require('express-validator');
-// const path = require('path');
-// const createMiddleware = [
-
-//     body('name')
-//         .notEmpty().withMessage('Por favor ingrese su nombre').bail()
-//         .isLength({min: 2}).withMessage('Minimo 2 caracteres')
-//         .isLength({max: 60}).withMessage('Maximo 20 caracteres'),
-//     body('description')
-//         .notEmpty().withMessage('Por favor ingrese una descripcion').bail()
-//         .isLength({min: 60}).withMessage('Minimo 60 caracteres')
-//         .isLength({max: 3500}).withMessage('Maximo 500 caracteres'),
-//     body('price')
-//         .notEmpty().withMessage('Por favor ingrese un precio').bail()
-//         .isNumeric().withMessage('Por favor ingrese un valor numerico'),
-//     body('image')
-//         .custom((val,{req})=>{
-//             let validExtension = ['.jpg','.png', '.svg']
-//             let file = req.file
-//             if(!file){
-//                 throw new Error('Ingrese una imagen')
-//             }
-//             else{
-//                 let extension = path.extname(file.originalname)
-//                 if(extension.includes(validExtension)){
-//                     throw new Error(`Las Extenciones validas son:${extension.join(', ')}`)
-//                 }
-//             }
-//                 return true
-//         })
-
-
-// ];
-
-// module.exports = createMiddleware
-
-
-
 const {body} = require('express-validator');
 const path = require('path');
 const createMiddleware = [
@@ -43,7 +5,11 @@ const createMiddleware = [
     body('name')
         .notEmpty().withMessage('Por favor ingrese su nombre').bail()
         .isLength({min: 2}).withMessage('Minimo 2 caracteres')
-        .isLength({max: 20}).withMessage('Maximo 20 caracteres'),
+        .isLength({max: 60}).withMessage('Maximo 20 caracteres'),
+    body('description')
+        .notEmpty().withMessage('Por favor ingrese una descripcion').bail()
+        .isLength({min: 60}).withMessage('Minimo 60 caracteres')
+        .isLength({max: 3500}).withMessage('Maximo 500 caracteres'),
     body('price')
         .notEmpty().withMessage('Por favor ingrese un precio').bail()
         .isNumeric().withMessage('Por favor ingrese un valor numerico'),
@@ -67,3 +33,4 @@ const createMiddleware = [
 ];
 
 module.exports = createMiddleware
+
