f1e50afad4287634992bbb5d993cb5c1e649e40a
Matias Chaves <matiaschaves01@gmail.com>
2024-07-07 22:12:23 -0300

    porfin base de datos arreglada

--- src/database/models/ProductSubCategory.js
+++ src/database/models/ProductSubCategory.js
@@ -1,31 +1,29 @@
 module.exports = (sequelize, DataType) => {
-    const ProductSubCategory = sequelize.define('ProductSubCategory',
-        {
-            id: {
-                type: DataType.INTEGER.UNSIGNED,
-                primaryKey: true,
-                autoIncrement: true
-            },
-            subcategory:{
-                type: DataType.STRING(255),
-            },
-        },
-        {
-            tableName:'product_subcategory',
-            timestamp: true,
-            underscored: true
-        }
-    )
+  const ProductSubCategory = sequelize.define(
+    "ProductSubCategory",
+    {
+      id: {
+        type: DataType.INTEGER.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      subcategory: {
+        type: DataType.STRING(255),
+      },
+    },
+    {
+      tableName: "product_subcategory",
+      timestamp: true,
+      underscored: true,
+    }
+  );
 
-    ProductSubCategory.associate = (models) => {
-        ProductSubCategory.hasMany(models.Product, {
-            as: 'Product',
-            foreingKey: 'product_sub_category_id'
-        })
-    };
+  ProductSubCategory.associate = (models) => {
+    ProductSubCategory.hasMany(models.Product, {
+      as: "Product",
+      foreingKey: "product_sub_category_id",
+    });
+  };
 
-        return ProductSubCategory
-}
-
-
-
+  return ProductSubCategory;
+};
--- src/database/script/fufu.sql
+++ src/database/script/fufu.sql
@@ -1,80 +1,75 @@
-drop database if exists `fufu_db`;
-create database if not exists `fufu_db`;
-use `fufu_db`;
-
-CREATE TABLE `products` (
-   `id` INT AUTO_INCREMENT,
-   `name` VARCHAR(255) NOT NULL,
-   `price` DECIMAL NOT NULL,
-   `image` VARCHAR(255),
-   `capacity` VARCHAR(255),
-   `description` VARCHAR(255),
-   `product_color_id` INT,
-   `product_sub_category_id` INT,
-   `product_categories_id` INT,
-   `created_at` TIMESTAMP DEFAULT current_timestamp,
-   `updated_at` TIMESTAMP DEFAULT current_timestamp,
-   `deleted_at` TIMESTAMP DEFAULT null,
-   PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `user` (
-   `id` INT NOT NULL AUTO_INCREMENT,
-   `name` VARCHAR(255),
-   `lastname` VARCHAR(255),
-   `username` VARCHAR(255) NOT NULL,
-   `phone` VARCHAR(255),
-   `email` VARCHAR(255) NOT NULL,
-   `image` VARCHAR(255),
-   `password` VARCHAR(255) NOT NULL,
-   `user_category_id` INT,
-   `created_at` TIMESTAMP DEFAULT current_timestamp,
-   `updated_at` TIMESTAMP DEFAULT current_timestamp,
-   `deleted_at` TIMESTAMP DEFAULT null,
-   PRIMARY KEY (`id`)
-);
+DROP DATABASE IF EXISTS `fufu_db`;
+CREATE DATABASE IF NOT EXISTS `fufu_db`;
+USE `fufu_db`;
 
 CREATE TABLE `product_categories` (
-   `id` INT NOT NULL AUTO_INCREMENT,
-   `category` VARCHAR(255),
-   `created_at` TIMESTAMP DEFAULT current_timestamp,
-   `updated_at` TIMESTAMP DEFAULT current_timestamp,
-   `deleted_at` TIMESTAMP DEFAULT null,
-   PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `category` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT current_timestamp,
+    `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    `deleted_at` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `user_category` (
-   `id` INT NOT NULL AUTO_INCREMENT,
-   `category` VARCHAR(255),
-   `created_at` TIMESTAMP DEFAULT current_timestamp,
-   `updated_at` TIMESTAMP DEFAULT current_timestamp,
-   `deleted_at` TIMESTAMP DEFAULT null,
-   PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `category` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT current_timestamp,
+    `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    `deleted_at` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `product_subcategory` (
-   `id` INT NOT NULL AUTO_INCREMENT,
-   `subCategory` VARCHAR(255),
-   `created_at` TIMESTAMP DEFAULT current_timestamp,
-   `updated_at` TIMESTAMP DEFAULT current_timestamp,
-   `deleted_at` TIMESTAMP DEFAULT null,
-   PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `subCategory` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT current_timestamp,
+    `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    `deleted_at` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `product_colors` (
-   `id` INT NOT NULL AUTO_INCREMENT,
-   `color` VARCHAR(255),
-   `created_at` TIMESTAMP DEFAULT current_timestamp,
-   `updated_at` TIMESTAMP DEFAULT current_timestamp,
-   `deleted_at` TIMESTAMP DEFAULT null,
-   PRIMARY KEY (`id`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `color` VARCHAR(255) NOT NULL,
+    `created_at` TIMESTAMP DEFAULT current_timestamp,
+    `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    `deleted_at` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
 
+CREATE TABLE `products` (
+    `id` INT AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `image` VARCHAR(255),
+    `capacity` VARCHAR(255),
+    `description` VARCHAR(255),
+    `product_color_id` INT,
+    `product_sub_category_id` INT,
+    `product_categories_id` INT,
+    `created_at` TIMESTAMP DEFAULT current_timestamp,
+    `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    `deleted_at` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`product_color_id`) REFERENCES `product_colors`(`id`),
+    FOREIGN KEY (`product_sub_category_id`) REFERENCES `product_subcategory`(`id`),
+    FOREIGN KEY (`product_categories_id`) REFERENCES `product_categories`(`id`)
+);
 
-ALTER TABLE `products` ADD CONSTRAINT `FK_3f540d22-b362-4089-9725-7fb1c55659f1` FOREIGN KEY (`product_categories_id`) REFERENCES `product_categories`(`id`)  ;
-
-ALTER TABLE `products` ADD CONSTRAINT `FK_b76a87d0-fef1-45f3-a4ff-42e7995c4d3b` FOREIGN KEY (`product_sub_category_id`) REFERENCES `product_subcategory`(`id`)  ;
-
-ALTER TABLE `products` ADD CONSTRAINT `FK_b4d05190-86be-4485-8852-31191d908e6d` FOREIGN KEY (`product_color_id`) REFERENCES `product_colors`(`id`)  ;
-
-ALTER TABLE `user` ADD CONSTRAINT `FK_24c63f8e-6568-47ab-a485-01dc97ebe2b9` FOREIGN KEY (`user_category_id`) REFERENCES `user_category`(`id`)  ;
+CREATE TABLE `user` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `lastname` VARCHAR(255),
+    `username` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(255),
+    `email` VARCHAR(255) NOT NULL,
+    `image` VARCHAR(255),
+    `password` VARCHAR(255) NOT NULL,
+    `user_category_id` INT,
+    `created_at` TIMESTAMP DEFAULT current_timestamp,
+    `updated_at` TIMESTAMP DEFAULT current_timestamp ON UPDATE current_timestamp,
+    `deleted_at` DATETIME DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_category_id`) REFERENCES `user_category`(`id`)
+);
